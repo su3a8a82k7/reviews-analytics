b1df2256f05d31cbc2e72812f068fca8161f2579
George Chang <su3a8a82k7@gmail.com>
2020-04-20 14:44:24 +0700

    add the average

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open('original.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))		
-print(len(data))
+print('讀取完了, 總共有', len(data), '筆')
 
-print(data[0])
-print('----------------------------')
-print(data[1])		
+sum_len = 0
+for d in data:
+	sum_len +=  len(d)
+	
+print('留言平均字數為:', sum_len/len(data))
